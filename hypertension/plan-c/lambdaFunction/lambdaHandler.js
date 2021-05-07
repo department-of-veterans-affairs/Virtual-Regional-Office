d6fbe2bff0c47d02bb61b19e3e1c96a26979acdc
Aaron Houlihan <aaron@amida.com>
2021-05-06 21:35:56 -0400

    [env vars] Lambda fetch secret from SecretsManager

--- hypertension/plan-c/lambdaFunction/lambdaHandler.js
+++ hypertension/plan-c/lambdaFunction/lambdaHandler.js
@@ -1,6 +1,35 @@
-exports.handler = async (event) {
+const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager')
+
+exports.handler = async (event) => {
+
+  const { LH_CLIENT_ID, LH_PRIVATE_RSA_KEY_SECRET_ARN } = process.env
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello world!')
+    body: {
+      LH_CLIENT_ID,
+      secret: await fetchLhPrivateRsaKey(LH_PRIVATE_RSA_KEY_SECRET_ARN)
+    }
+  }
+  return response
+}
+
+async function fetchLhPrivateRsaKey (LH_PRIVATE_RSA_KEY_SECRET_ARN) {
+
+  const smClient = new SecretsManagerClient({region: 'us-west-2'})
+
+  const params = {
+    SecretId: LH_PRIVATE_RSA_KEY_SECRET_ARN
+  }
+
+  const command = new GetSecretValueCommand(params)
+
+  try {
+    const data = await smClient.send(command)
+    return data.SecretString
+  }
+  catch (err) {
+    console.log(err, err.stack)
+    return err
   }
 }
