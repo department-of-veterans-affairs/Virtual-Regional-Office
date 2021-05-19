66822c08452e62b4f4d209d52fff8ae7e652db10
Aaron Houlihan <aaron@amida.com>
2021-05-18 23:18:56 -0400

    [env vars] Python Lambda fetch secret from SecretsManager

--- hypertension/plan-b/README.md
+++ hypertension/plan-b/README.md
@@ -1,8 +1,22 @@
+## WARNING
+
+DO NOT ENTER YOUR REAL LIGHTHOUSE RSA PRIVATE KEY INTO THE ENVIRONMENT VARIABLE! The Pthon code currently prints it to the logs and also returns it in the body of the Lambda response.
+
+## Develop
+
+### Step 1
+Create and fill out our cloud formation template variables
+```sh
+cp cf-template-params-example.env cf-template-params.env
+```
+
 ## Run in the Cloud
+
+Do Develop >>> Step 1
 
 ```sh
 make build.sam.cloud
-sam deploy --guided
+sam deploy --guided --capabilities CAPABILITY_NAMED_IAM --parameter-overrides $(cat cf-template-params.env)
 ```
 
 ## Run Locally
--- hypertension/plan-b/lambda/app.py
+++ hypertension/plan-b/lambda/app.py
@@ -1,7 +1,29 @@
+import os
+
+import boto3
+
+smClient = boto3.client('secretsmanager')
+
 def lambda_handler(event, context):
     print(f"Incoming event: {event}")
 
+    print('## ENVIRONMENT VARIABLES')
+    print(os.environ['LH_CLIENT_ID'])
+
+    rsaKey = get_lh_rsa_key()
+
     return {
         'statusCode': 200,
-        'body': event
+        # TODO: Fix this. WARNING this is not safe. Don't use this code with a real secret. It's just like this for testing purposes.
+        'body': rsaKey
     }
+
+def get_lh_rsa_key():
+    result = smClient.get_secret_value(SecretId=os.environ['LH_PRIVATE_RSA_KEY_SECRET_ARN'])
+
+    # TODO: Fix this. WARNING this is not safe. Don't use this code with a real secret. It's just like this for testing purposes.
+    print('## Result from secret fetch')
+    print(result)
+    print(result['SecretString'])
+
+    return result['SecretString']
