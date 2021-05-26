42ab9028a33172fbf532163b7f252a382c3f5d08
Aaron Houlihan <aaron@amida.com>
2021-05-26 17:25:04 -0400

    [py lamb scratch] add python requests library

--- hypertension/plan-1-py-lamb-from-scratch/vro-main/app.py
+++ hypertension/plan-1-py-lamb-from-scratch/vro-main/app.py
@@ -1,9 +1,15 @@
 import json
+
+import requests
+
 def lambda_handler(event, context):
+
+    res = requests.get('https://httpbin.org/get')
+
+    print('res.text is')
+    print(res.text)
+
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        "body": res.text
     }
