a383ec12b7d38a6e1d1f7115000375b36f128205
Aaron Houlihan <aaron@amida.com>
2021-06-03 19:43:27 -0400

    [env vars] fetch LH RSA key from AWS SecMgr, and mock it for local

--- hypertension/vro-main/test/main_test.py
+++ hypertension/vro-main/test/main_test.py
@@ -1,7 +1,10 @@
+import os
 from lib.main import main
+
+from test.doubles.aws_secrets_manager import mock_get_lighthouse_rsa_key
 
 def test_main():
   assert main() == {
     "statusCode": 200,
-    "body": "Hello world!"
+    "body": mock_get_lighthouse_rsa_key('Fake. Doesnt Matter. Just need something here.')
   }
