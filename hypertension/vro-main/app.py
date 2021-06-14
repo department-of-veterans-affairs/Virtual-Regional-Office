fa4b3bd2f9d77e823abf1050d6770e773a8016fb
Julia Allen <julia.l.allen@gsa.gov>
2021-06-14 15:40:26 -0700

    test pdfkit can output a pdf from lambda

--- hypertension/vro-main/app.py
+++ hypertension/vro-main/app.py
@@ -1,6 +1,12 @@
 from lib.main import main
 import pdfkit
 
+
 def lambda_handler(event, context):
-  pdf = pdfkit.from_file(event.file, 'test-html.pdf')
-  print(pdf, file='test-pdf-output.pdf')
+    # testing out pdfkit
+    config = pdfkit.configuration(wkhtmltopdf='/opt/bin/wkhtmltopdf')
+    pdf = pdfkit.from_url(event.get('url'), None, configuration=config)
+
+    print(pdf)
+
+    return main()
