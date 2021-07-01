b1155fc0950332310bd27af5e91e404365f38f47
Aaron Houlihan <aaron@amida.com>
2021-07-01 18:52:41 -0400

    [move scripts] first pass at load_config

--- hypertension/py-root/lib/utils.py
+++ hypertension/py-root/lib/utils.py
@@ -1,3 +1,5 @@
+from scripts.python_get_token_make_api_request_script.get_token_make_api_request import get_cli_args
+
 # AWS SAM CLI can't (easily) take parameters that are multi-line or have
 # spaces. Thus, any private RSA key we need must be ingested as a single line
 # with no spaces, and converted back to normal, multi-line PEM format via
@@ -12,3 +14,13 @@
 
 def fix_pem_formatting(custom_format_rsa_key):
     return custom_format_rsa_key.replace("_", " ").replace("&", "\n")
+
+
+def load_config(running_as_script) -> dict:
+    if running_as_script:
+        config = "TO BE IMPLEMENTED"
+        opts = get_cli_args(['myClientId', 'myKeyLoc', 'myAssertionsFile', 'myParamsFile', 'myIcn' ])
+    else:
+        config = "TO BE IMPLEMENTED"
+
+    return config
--- hypertension/py-root/test/utils_test.py
+++ hypertension/py-root/test/utils_test.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from lib.utils import fix_pem_formatting
 
+from lib.utils import load_config
 
 CURRENT_DIR = Path(__file__).parent
 NORMAL_RSA_KEY_PATH = CURRENT_DIR / "fake-realistic-rsa-key.txt"
@@ -12,3 +13,6 @@ def test_fix_pem_formatting():
     key_in_our_custom_format = CUSTOM_RSA_KEY_PATH.read_text().strip()
 
     assert fix_pem_formatting(key_in_our_custom_format) == normal_rsa_key
+
+def test_load_config():
+    assert load_config(True) == "TO BE IMPLEMENTED"
