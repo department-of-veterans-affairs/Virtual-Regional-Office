c43b6deb002b87f1cf2bb0dd4a888e741e225908
Aaron Houlihan <aaron@amida.com>
2021-07-02 18:56:06 -0400

    [move scripts] get config from load_config

--- hypertension/py-root/lib/utils.py
+++ hypertension/py-root/lib/utils.py
@@ -1,7 +1,3 @@
-from scripts.python_get_token_make_api_request_script.get_token_make_api_request import (
-    get_cli_args,
-)
-
 # AWS SAM CLI can't (easily) take parameters that are multi-line or have
 # spaces. Thus, any private RSA key we need must be ingested as a single line
 # with no spaces, and converted back to normal, multi-line PEM format via
@@ -16,37 +12,3 @@ from scripts.python_get_token_make_api_request_script.get_token_make_api_request
 
 def fix_pem_formatting(custom_format_rsa_key):
     return custom_format_rsa_key.replace("_", " ").replace("&", "\n")
-
-
-def load_config(running_as_script) -> dict:
-    if running_as_script:
-        config = "TO BE IMPLEMENTED"
-        opts = get_cli_args(
-            [
-                "myClientId",
-                "myKeyLoc",
-                "myAssertionsFile",
-                "myParamsFile",
-                "myIcn",
-            ]
-        )
-        # config["auth_data"] = load_json(opts.assertions_file)
-        # config["api_data"] = load_json(opts.params_file)
-        # config["auth_data"]["secret"] = load_secret(opts.key_loc)
-    else:
-        config = "TO BE IMPLEMENTED"
-    #     config = {
-    #         "auth_data": {
-    #             "urls": {
-    #                 "audience": os.environ["LighthouseJwtAudUrl"],
-    #                 "token": os.environ["LighthouseTokenUrl"]
-    #             },
-    #             "parameters": {
-    #                 "grant_type": os.environ["LighthouseOauthGrantType"],
-    #                 "client_assertion_type": os.environ["LighthouseOauthClientAssertionType"],
-    #                 "scope": "launch/patient patient/Patient.read patient/Observation.read patient/Medication.read"
-    #             }
-    #         }
-    #     }
-    # }
-    return config
