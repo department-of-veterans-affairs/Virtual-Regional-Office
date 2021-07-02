9b5df9a163c4c3573df6f644487ebfcfc261aa22
Aaron Houlihan <aaron@amida.com>
2021-07-01 21:47:15 -0400

    [move scripts] pytest lighthouse scripts, rev 1

--- hypertension/py-root/test/conftest.py
+++ hypertension/py-root/test/conftest.py
@@ -1,8 +1,13 @@
+import sys
 import os
 from test.doubles.aws_secrets_manager import get_lighthouse_rsa_key_double
 from dotenv import load_dotenv
 import lib.aws_secrets_manager as aws_secrets_manager
-
+from test.doubles.filesystem_operations import (
+    load_text_double,
+    load_secret_double,
+)
+import scripts.python_get_token_make_api_request_script.get_token_make_api_request as get_token_make_api_request
 
 load_dotenv("../cf-template-params.env")
 
@@ -12,3 +17,16 @@ os.environ[
 
 # Mock AWS::SecretsManager::Secret fetch
 aws_secrets_manager.get_lighthouse_rsa_key = get_lighthouse_rsa_key_double
+
+# Mock read files from filesystem
+get_token_make_api_request.load_text = load_text_double
+get_token_make_api_request.load_secret = load_secret_double
+
+sys.argv = [
+    "scripts/python_get_token_make_api_request_script/get_token_make_api_request.py",
+    "FakeLighthouseClientCredentialsOAuthClientId",
+    "FakePathToLighthouseRsaPrivateKeyPemFile",
+    "FakePathToLighthouseAuthAssertionsFile",
+    "FakePathToLighthouseHealthApiObservationRequestParamsFile",
+    "FakeVeteranIcn",
+]
