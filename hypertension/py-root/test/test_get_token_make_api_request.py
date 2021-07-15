a1b5a4ed7289217613c705f128f076c94c5045bc
Aaron Houlihan <aaron@amida.com>
2021-07-14 21:31:43 -0400

    [move scripts] Fix pytest CLI args ingestion and test LH auth

--- hypertension/py-root/test/test_get_token_make_api_request.py
+++ hypertension/py-root/test/test_get_token_make_api_request.py
@@ -1,10 +1,4 @@
-from scripts.python_get_token_make_api_request_script.get_token_make_api_request import (
-    cli_main,
-)
-
-
-def test_get_token_make_api_request():
-    result = cli_main()
-    print("result is -------------------------------------------------")
-    print(result)
-    assert 1 == 1
+# TODO: Mock the API call
+# TODO: Test something better than the length of the access token
+def test_authenticate_to_lighthouse(lh_access_token):
+    assert len(lh_access_token) == 929
