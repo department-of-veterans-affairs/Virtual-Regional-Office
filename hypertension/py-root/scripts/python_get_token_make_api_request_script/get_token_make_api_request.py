faa134e472f96fbde73146a0098a75bf2f7bfe4f
Aaron Houlihan <aaron@amida.com>
2021-07-14 21:31:43 -0400

    [move scripts] Fix pytest CLI args ingestion and test LH auth

--- hypertension/py-root/scripts/python_get_token_make_api_request_script/get_token_make_api_request.py
+++ hypertension/py-root/scripts/python_get_token_make_api_request_script/get_token_make_api_request.py
@@ -53,7 +53,7 @@ def cli_main() -> None:
 
     icn = config["lighthouse"]["icn"]
 
-    access_token= authenticate_to_lighthouse(config["lighthouse"]["auth"], icn)
+    access_token = authenticate_to_lighthouse(config["lighthouse"]["auth"], icn)
 
     observation_response = fetch_observation_data(config["lighthouse"]["vet_health_api_observation"], icn, access_token)
 
--- hypertension/py-root/test/conftest.py
+++ hypertension/py-root/test/conftest.py
@@ -1,9 +1,15 @@
+import pytest
 import sys
 import os
 from test.doubles.aws_secrets_manager import get_lighthouse_rsa_key_double
 from dotenv import load_dotenv
 import lib.aws_secrets_manager as aws_secrets_manager
 import scripts.python_get_token_make_api_request_script.get_token_make_api_request as get_token_make_api_request
+from scripts.python_get_token_make_api_request_script.get_token_make_api_request import (
+    load_config,
+    authenticate_to_lighthouse
+)
+from test.doubles.get_token_make_api_request import get_cli_args_double
 
 load_dotenv("../cf-template-params.env")
 
@@ -14,11 +20,18 @@ os.environ[
 # Mock AWS::SecretsManager::Secret fetch
 aws_secrets_manager.get_lighthouse_rsa_key = get_lighthouse_rsa_key_double
 
-sys.argv = [
-    "scripts/python_get_token_make_api_request_script/get_token_make_api_request.py",
-    os.environ["LighthouseClientId"],
-    os.environ["LighthousePrivateRsaKeyFilePath"],
-    "./scripts/python_get_token_make_api_request_script/assertion-params-example.json",
-    "./scripts/python_get_token_make_api_request_script/observation-request-params-example.json",
-    os.environ["TestVeteranIcn"],
-]
+
+# TODO: Scope this to the session.
+# You'll have to deal with the problem of trying to use monkey patch in a thing that's not function scoped
+# https://github.com/pytest-dev/pytest/issues/1872
+@pytest.fixture()
+def lh_access_token(monkeypatch):
+    monkeypatch.setattr(get_token_make_api_request, 'get_cli_args', get_cli_args_double)
+
+    config = load_config(True)
+
+    icn = config["lighthouse"]["icn"]
+
+    access_token = authenticate_to_lighthouse(config["lighthouse"]["auth"], icn)
+
+    return access_token
