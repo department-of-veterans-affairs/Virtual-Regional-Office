9758d0bc0ea5b50451f1962b1ec925efa5664bb5
Aaron Houlihan <aaron@amida.com>
2021-07-14 21:29:06 -0400

    [move scripts] Remove useless load_config test

--- hypertension/py-root/test/utils_test.py
+++ hypertension/py-root/test/utils_test.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 from lib.utils import fix_pem_formatting
 
-from lib.utils import load_config
-
 CURRENT_DIR = Path(__file__).parent
 NORMAL_RSA_KEY_PATH = CURRENT_DIR / "fake-realistic-rsa-key.txt"
 CUSTOM_RSA_KEY_PATH = CURRENT_DIR / "fake-custom-rsa-key.txt"
@@ -13,7 +11,3 @@ def test_fix_pem_formatting():
     key_in_our_custom_format = CUSTOM_RSA_KEY_PATH.read_text().strip()
 
     assert fix_pem_formatting(key_in_our_custom_format) == normal_rsa_key
-
-
-def test_load_config():
-    assert 1 == 1
