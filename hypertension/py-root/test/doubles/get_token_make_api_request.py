855fe85db190c1306924470ba7ccbbec94c6ea6c
Aaron Houlihan <aaron@amida.com>
2021-07-20 22:08:58 -0400

    [move scripts] variable renames

--- hypertension/py-root/test/doubles/get_token_make_api_request.py
+++ hypertension/py-root/test/doubles/get_token_make_api_request.py
@@ -8,7 +8,7 @@ from scripts.python_get_token_make_api_request_script.get_token_make_api_request
 def get_cli_args_double() -> argparse.Namespace:
     return setup_cli_parser().parse_args([
         # "scripts/python_get_token_make_api_request_script/get_token_make_api_request.py",
-        os.environ["LighthouseClientId"],
+        os.environ["LighthouseOAuthClientId"],
         os.environ["LighthousePrivateRsaKeyFilePath"],
         "./scripts/python_get_token_make_api_request_script/assertion-params-example.json",
         "./scripts/python_get_token_make_api_request_script/observation-request-params-example.json",
