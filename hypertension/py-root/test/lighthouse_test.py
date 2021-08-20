e3518cbc6af8f3b9c77349c37a958d5020a9a1e4
Aaron Houlihan <aaron@amida.com>
2021-08-20 18:36:56 -0400

    [move scripts] add test_test_fetch_observation_data

--- hypertension/py-root/test/lighthouse_test.py
+++ hypertension/py-root/test/lighthouse_test.py
@@ -1,5 +1,23 @@
+import lib.lighthouse as lighthouse
+
+from lib.lighthouse import fetch_observation_data
+from test.doubles.lighthouse import http_get_api_request_double
+
+from test.data.lighthouse import (
+    lh_observation_response
+)
+
 # TODO: Mock the API call
 # TODO: Test something better than the length of the access token
 def test_authenticate_to_lighthouse(lh_access_token):
     length_of_a_lh_access_token = 929
     assert len(lh_access_token) == length_of_a_lh_access_token
+
+def test_fetch_observation_data(monkeypatch_session, config, lh_access_token):
+    monkeypatch_session.setattr(lighthouse, 'http_get_api_request', http_get_api_request_double)
+
+    observation_response = fetch_observation_data(
+        config["lighthouse"]["vet_health_api_observation"], config["lighthouse"]["icn"], lh_access_token
+    )
+
+    assert lh_observation_response == observation_response
