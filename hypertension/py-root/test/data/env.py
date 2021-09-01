a3a57a6e676d49a49bff5c0b8f02754916944460
Aaron Houlihan <aaron@amida.com>
2021-09-01 18:40:26 -0400

    [move scripts] Final TODOs for this PR

---
+++ hypertension/py-root/test/data/env.py
@@ -0,0 +1,12 @@
+fake_env = {
+    "LighthouseTokenUrl": "DUMMY_DONT_CARE",
+    "LighthouseJwtAudUrl": "DUMMY_DONT_CARE",
+    "LighthouseJwtScope": "DUMMY_DONT_CARE",
+    "LighthouseOAuthClientId": "DUMMY_DONT_CARE",
+    "LighthouseOAuthGrantType": "DUMMY_DONT_CARE",
+    "LighthouseOAuthAssertionType": "DUMMY_DONT_CARE",
+    "KmsCmkId": "DUMMY_DONT_CARE",
+    "LighthouseObservationUrl": "DUMMY_DONT_CARE",
+    "LighthouseObservationCategory": "DUMMY_DONT_CARE",
+    "LighthouseObservationLoincCode": "DUMMY_DONT_CARE",
+}
--- hypertension/py-root/test/scripts_test.py
+++ hypertension/py-root/test/scripts_test.py
@@ -2,26 +2,14 @@ import os
 import re
 from pathlib import Path
 from unittest import mock
+from test.data.env import fake_env
 from set_parameter_overrides import set_parameter_overrides
 
-# TODO: Figure out where to put this. Make a const?
-fake_env = {
-    "LighthouseTokenUrl": "DUMMY_DONT_CARE",
-    "LighthouseJwtAudUrl": "DUMMY_DONT_CARE",
-    "LighthouseJwtScope": "DUMMY_DONT_CARE",
-    "LighthouseOAuthClientId": "DUMMY_DONT_CARE",
-    "LighthouseOAuthGrantType": "DUMMY_DONT_CARE",
-    "LighthouseOAuthAssertionType": "DUMMY_DONT_CARE",
-    "KmsCmkId": "DUMMY_DONT_CARE",
-    "LighthouseObservationUrl": "DUMMY_DONT_CARE",
-    "LighthouseObservationCategory": "DUMMY_DONT_CARE",
-    "LighthouseObservationLoincCode": "DUMMY_DONT_CARE",
-}
 
-
-# TODO: Parametrize this test?
-# TODO: Break this out into test_update_file() and take that part out of
-# test_set_parameter_overrides()? Or just keep it as is because that's simpler?
+# TODO: Add test for present and absent newline character after the
+# `parameter_overrides` line
+# TODO: Add test for when there's a line in the samconfig.toml file that is
+# _after_ the parameter_overrides line
 @mock.patch.dict(os.environ, fake_env)
 def test_set_parameter_overrides():
     # Get actual
@@ -30,12 +18,6 @@ def test_set_parameter_overrides():
     actual = Path(samconfig_file_path).read_text()
 
     # Construct expected
-    # TODO: Instead of this should we do the following?
-    # - filesystem cp the root dir samconfig-default.toml into your test dir,
-    # - then read it into here
-    # - then do your operations on the copied version of samconfig-default.toml
-    # - then finish this test
-    # - then delete the copied version of samconfig-default.toml
     samconfig_default_content = """version = 0.1
 [default]
 [default.deploy]
@@ -75,9 +57,3 @@ parameter_overrides = "DUMMY_VALUE___OVERWRITE_BEFORE_YOU_DEPLOY"
 
     # Cleanup
     Path(samconfig_file_path).write_text(samconfig_default_content)
-
-
-# TODO: Add test for present and absent newline character after the
-# `parameter_overrides` line
-# TODO: Add test for when there's a line in the samconfig.toml file that is
-# _after_ the parameter_overrides line
