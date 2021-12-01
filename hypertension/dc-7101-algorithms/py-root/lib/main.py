37ccdb9b024a9d64b853be289feba775e7dfe568
Aaron Houlihan <aaron@amida.com>
2021-11-30 19:56:51 -0500

    [MCP-1017] Add CORS headers to lambda response

--- hypertension/dc-7101-algorithms/py-root/lib/main.py
+++ hypertension/dc-7101-algorithms/py-root/lib/main.py
@@ -1,4 +1,12 @@
 from typing import Dict
 
 def main(event: Dict):
-    return {"statusCode": 200, "body": "test body"}
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST"
+        },
+        "body": "test body"
+    }
--- hypertension/dc-7101-algorithms/py-root/test/main_test.py
+++ hypertension/dc-7101-algorithms/py-root/test/main_test.py
@@ -2,4 +2,13 @@ from lib.main import main
 
 def test_main():
     test_event = {"irrelevant": "value"}
-    assert main(test_event) == {"statusCode": 200, "body": "test body"}
+    assert main(test_event) == {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST"
+        },
+        "body": "test body"
+    }
+
