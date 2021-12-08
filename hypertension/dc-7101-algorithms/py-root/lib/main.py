2a3431186761ad35b956a0d8a2109241116e76cd
tylernoblett <tyler.noblett@outlook.com>
2021-12-08 11:38:58 -0500

    remove duplicate code

--- hypertension/dc-7101-algorithms/py-root/lib/main.py
+++ hypertension/dc-7101-algorithms/py-root/lib/main.py
@@ -2,8 +2,8 @@ from typing import Dict
 from .utils import sufficient_to_autopopulate, history_of_diastolic_bp
 
 def main(event: Dict):
+    statusCode = 200
     try:
-        statusCode = 200
         predominance_calculation = sufficient_to_autopopulate(event)
         diastolic_history_calculation = history_of_diastolic_bp(event)
         predominance_calculation_status = predominance_calculation["success"]
@@ -20,7 +20,12 @@ def main(event: Dict):
         elif not predominance_calculation_status and not diastolic_history_calculation_status:
             statusCode = 400
 
-        return {
+    except Exception as e:
+        statusCode = 500
+        predominance_calculation = {"success": False}
+        diastolic_history_calculation = {"success": False}
+
+    return {
             "statusCode": statusCode,
             "headers": {
                 "Access-Control-Allow-Headers" : "Content-Type",
@@ -32,22 +37,6 @@ def main(event: Dict):
                 "diastolic_history_calculation": diastolic_history_calculation 
             }
         }
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "headers": {
-                "Access-Control-Allow-Headers" : "Content-Type",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "OPTIONS,POST"
-            },
-            "body": {
-                "predominance_calculation": {
-                        "success": False,
-                    },
-                    "diastolic_history_calculation": {
-                        "success": False 
-                    } 
-            }
-        }
+
 
 
