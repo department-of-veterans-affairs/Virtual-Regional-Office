920ebfb3e79a63ffc13908b8327fde77c6799318
tylernoblett <tyler.noblett@outlook.com>
2021-12-08 10:04:11 -0500

    parametrize main test

--- hypertension/dc-7101-algorithms/py-root/test/main_test.py
+++ hypertension/dc-7101-algorithms/py-root/test/main_test.py
@@ -1,25 +1,51 @@
+import pytest
 from lib.main import main
 from test.data.htn_data import general_request_data
 
-def test_main():
-    assert main(general_request_data) == {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST"
-        },
-        "body": {
-            "predominance_calculation": {
-                'sufficient_to_autopopulate': True,
-                "success": True,
-                'predominant_diastolic_reading': 115,
-                'predominant_systolic_reading': 180
+@pytest.mark.parametrize(
+    "request_data, response",
+    [
+        # 0 readings
+        (
+            {
+                "bp": [
+                    {
+                        "diastolic": 115,
+                        "systolic": 180,
+                        "date": "2021-11-01"
+                    },
+                    {
+                        "diastolic": 110,
+                        "systolic": 200,
+                        "date": "2021-09-01"
+                    }
+                ],
+                "medication": [],
+                'date_of_claim': '2021-11-09',
             },
-            "diastolic_history_calculation": {
-                "diastolic_bp_predominantly_100_or_more": True,
-                "success": True 
+            {
+                "statusCode": 200,
+                "headers": {
+                    "Access-Control-Allow-Headers" : "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST"
+                },
+                "body": {
+                    "predominance_calculation": {
+                        'sufficient_to_autopopulate': True,
+                        "success": True,
+                        'predominant_diastolic_reading': 115,
+                        'predominant_systolic_reading': 180
+                    },
+                    "diastolic_history_calculation": {
+                        "diastolic_bp_predominantly_100_or_more": True,
+                        "success": True 
+                    }
+                }
             }
-        }
-    }
+        ),
+    ],
+)
+def test_main(request_data, response):
+    assert main(request_data) == response
 
