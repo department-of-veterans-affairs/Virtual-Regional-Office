2ccc2b8064abe3730fc96dfdf7d9ffc849776b83
tylernoblett <tyler.noblett@outlook.com>
2021-12-08 10:03:56 -0500

    add additional logic to main

--- hypertension/dc-7101-algorithms/py-root/lib/main.py
+++ hypertension/dc-7101-algorithms/py-root/lib/main.py
@@ -2,15 +2,30 @@ from typing import Dict
 from .utils import sufficient_to_autopopulate, history_of_diastolic_bp
 
 def main(event: Dict):
+    statusCode = 200
+    predominance_calculation = sufficient_to_autopopulate(event)
+    diastolic_history_calculation = history_of_diastolic_bp(event)
+    predominance_calculation_status = predominance_calculation["success"]
+    diastolic_history_calculation_status = diastolic_history_calculation["success"]
+
+    if (
+        (predominance_calculation_status and not diastolic_history_calculation_status) 
+        or (not predominance_calculation_status and diastolic_history_calculation_status)
+    ):
+        statusCode = 209
+    elif not predominance_calculation_status and not diastolic_history_calculation_status:
+        statusCode = 400
+
+    # how ot return 500 server error?
     return {
-        "statusCode": 200,
+        "statusCode": statusCode,
         "headers": {
             "Access-Control-Allow-Headers" : "Content-Type",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "OPTIONS,POST"
         },
         "body": {
-            "predominance_calculation": sufficient_to_autopopulate(event),
-            "diastolic_history_calculation": history_of_diastolic_bp(event)
+            "predominance_calculation": predominance_calculation,
+            "diastolic_history_calculation": diastolic_history_calculation 
         }
     }
