a7f9e91399cc291afd43ef505e0c161a8fd46f60
tylernoblett <tyler.noblett@outlook.com>
2021-12-09 14:21:51 -0500

    add tests for the tally count functions

--- hypertension/dc-7101-algorithms/py-root/test/utils_test.py
+++ hypertension/dc-7101-algorithms/py-root/test/utils_test.py
@@ -1,29 +1,101 @@
 import pytest
-from lib.algorithms.utils import bp_readings_meet_date_specs, tally_diastolic_counts
+from lib.algorithms.utils import bp_readings_meet_date_specs, tally_diastolic_counts, tally_systolic_counts
 
-# @pytest.mark.parametrize(
-#     "bp_readings, result",
-#     [
-#         # Valid reading
-#         (
-#             [
-#                 {
-#                     "diastolic": 115,
-#                     "systolic": 180,
-#                     "date": "2021-10-10"
-#                 },
-#                 {
-#                     "diastolic": 110,
-#                     "systolic": 200,
-#                     "date": "2021-09-01"
-#                 }
-#             ],
-#         ),
-#     ],
-# )
-# def test_tally_diastolic_counts(bp_readings, result):
-#     assert tally_diastolic_counts(bp_readings) == result
+@pytest.mark.parametrize(
+    "bp_readings, result",
+    [
+        (
+            [
+                {
+                    "diastolic": 115,
+                    "systolic": 180,
+                    "date": "2021-10-10"
+                },
+                {
+                    "diastolic": 110,
+                    "systolic": 200,
+                    "date": "2021-09-01"
+                },
+                {
+                    "diastolic": 105,
+                    "systolic": 180,
+                    "date": "2021-10-10"
+                },
+                {
+                    "diastolic": 121,
+                    "systolic": 200,
+                    "date": "2021-09-01"
+                },
+                {
+                    "diastolic": 91,
+                    "systolic": 200,
+                    "date": "2021-09-01"
+                },
+                {
+                    "diastolic": 135,
+                    "systolic": 200,
+                    "date": "2021-09-01"
+                }
+            ],
+            {
+                "130": 1,
+                "120": 1,
+                "110": 2,
+                "100": 1,
+                "less_than_one_hundred": 1
+            }
+        ),
+    ],
+)
+def test_tally_diastolic_counts(bp_readings, result):
+    assert tally_diastolic_counts(bp_readings) == result
 
+@pytest.mark.parametrize(
+    "bp_readings, result",
+    [
+        (
+            [
+                {
+                    "diastolic": 115,
+                    "systolic": 150,
+                    "date": "2021-10-10"
+                },
+                {
+                    "diastolic": 110,
+                    "systolic": 159,
+                    "date": "2021-09-01"
+                },
+                {
+                    "diastolic": 105,
+                    "systolic": 160,
+                    "date": "2021-10-10"
+                },
+                {
+                    "diastolic": 121,
+                    "systolic": 170,
+                    "date": "2021-09-01"
+                },
+                {
+                    "diastolic": 91,
+                    "systolic": 201,
+                    "date": "2021-09-01"
+                },
+                {
+                    "diastolic": 135,
+                    "systolic": 200,
+                    "date": "2021-09-01"
+                }
+            ],
+            {
+                "200": 2,
+                "160": 2,
+                "less_than_one_sixty": 2
+            }
+        ),
+    ],
+)
+def test_tally_systolic_counts(bp_readings, result):
+    assert tally_systolic_counts(bp_readings) == result
 
 @pytest.mark.parametrize(
     "date_of_claim, bp_readings, result",
