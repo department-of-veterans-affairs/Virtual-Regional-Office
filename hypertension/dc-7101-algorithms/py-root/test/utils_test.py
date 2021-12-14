cd327c0f0475828f967a15386352ab47b00d4c3d
tylernoblett <tyler.noblett@outlook.com>
2021-12-14 09:09:01 -0500

    add test for calculate_reading_from_bucket_count

--- hypertension/dc-7101-algorithms/py-root/test/utils_test.py
+++ hypertension/dc-7101-algorithms/py-root/test/utils_test.py
@@ -1,5 +1,5 @@
 import pytest
-from lib.algorithms.utils import bp_readings_meet_date_specs, tally_diastolic_counts, tally_systolic_counts
+from lib.algorithms.utils import bp_readings_meet_date_specs, tally_diastolic_counts, tally_systolic_counts, calculate_reading_from_bucket_count
 
 @pytest.mark.parametrize(
     "bp_readings, result",
@@ -96,6 +96,124 @@ def test_tally_diastolic_counts(bp_readings, result):
 )
 def test_tally_systolic_counts(bp_readings, result):
     assert tally_systolic_counts(bp_readings) == result
+
+@pytest.mark.parametrize(
+    "bucket_count, bp_readings, type, result",
+    [
+        # Test diastolic readings
+        (
+            {
+                "130": 2,
+                "120": 1,
+                "110": 2,
+                "100": 1,
+                "less_than_one_hundred": 2
+            },
+            [
+                {
+                    "diastolic": 115,
+                    "systolic": 180,
+                    "date": "2021-10-10",
+                    "diastolic_key": '110'
+                },
+                {
+                    "diastolic": 110,
+                    "systolic": 200,
+                    "date": "2021-09-01",
+                    "diastolic_key": '110'
+                },
+                {
+                    "diastolic": 105,
+                    "systolic": 180,
+                    "date": "2021-10-10",
+                    "diastolic_key": '100'
+                },
+                {
+                    "diastolic": 121,
+                    "systolic": 200,
+                    "date": "2021-09-01",
+                    "diastolic_key": '120'
+                },
+                {
+                    "diastolic": 91,
+                    "systolic": 200,
+                    "date": "2021-09-01",
+                    "diastolic_key": 'less_than_one_hundred'
+                },
+                {
+                    "diastolic": 95,
+                    "systolic": 180,
+                    "date": "2021-10-01",
+                    "diastolic_key": 'less_than_one_hundred'
+                },
+                {
+                    "diastolic": 135,
+                    "systolic": 190,
+                    "date": "2021-09-01",
+                    "diastolic_key": '130'
+                },
+                {
+                    "diastolic": 140,
+                    "systolic": 200,
+                    "date": "2021-09-05",
+                    "diastolic_key": '130'
+                }
+            ],
+            'diastolic',
+            140
+        ),
+        # Test systolic readings
+        (
+            {
+                "200": 2,
+                "160": 2,
+                "less_than_one_sixty": 2
+            },
+            [
+                {
+                    "diastolic": 115,
+                    "systolic": 180,
+                    "date": "2021-10-10",
+                    "systolic_key": '160'
+                },
+                {
+                    "diastolic": 110,
+                    "systolic": 210,
+                    "date": "2021-09-01",
+                    "systolic_key": '200'
+                },
+                {
+                    "diastolic": 105,
+                    "systolic": 180,
+                    "date": "2021-10-10",
+                    "systolic_key": '160'
+                },
+                {
+                    "diastolic": 121,
+                    "systolic": 200,
+                    "date": "2021-09-02",
+                    "systolic_key": '200'
+                },
+                {
+                    "diastolic": 91,
+                    "systolic": 150,
+                    "date": "2021-09-01",
+                    "systolic_key": 'less_than_one_sixty'
+                },
+                {
+                    "diastolic": 95,
+                    "systolic": 155,
+                    "date": "2021-10-01",
+                    "systolic_key": 'less_than_one_sixty'
+                },
+            ],
+            'systolic',
+            200
+        ),
+    ],
+)
+def test_calculate_reading_from_bucket_count(bucket_count, bp_readings, type, result):
+    assert calculate_reading_from_bucket_count(bucket_count, bp_readings, type) == result
 
 @pytest.mark.parametrize(
     "date_of_claim, bp_readings, result",
