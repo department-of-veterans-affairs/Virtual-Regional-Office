21193bcc04e9a801f9fdca68ac052e9d818d8817
Aaron Houlihan <aaron@amida.com>
2021-12-19 18:21:51 -0500

    [tyler htn algs] Whitespace

--- hypertension/dc-7101-algorithms/py-root/lib/algorithms/bp_sufficiency.py
+++ hypertension/dc-7101-algorithms/py-root/lib/algorithms/bp_sufficiency.py
@@ -1,7 +1,12 @@
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-from .utils import tally_diastolic_counts, tally_systolic_counts, calculate_reading_from_bucket, bp_readings_meet_date_specs
+from .utils import (
+    tally_diastolic_counts,
+    tally_systolic_counts,
+    calculate_reading_from_bucket,
+    bp_readings_meet_date_specs
+)
 
 def sufficient_to_autopopulate (request):
     """
