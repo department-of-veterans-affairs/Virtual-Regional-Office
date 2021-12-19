e772923083dd5a015d109a6871800d1992866b40
Aaron Houlihan <aaron@amida.com>
2021-12-19 18:20:21 -0500

    [tyler htn algs] Variable rename

--- hypertension/dc-7101-algorithms/py-root/lib/algorithms/bp_sufficiency.py
+++ hypertension/dc-7101-algorithms/py-root/lib/algorithms/bp_sufficiency.py
@@ -18,7 +18,7 @@ def sufficient_to_autopopulate (request):
         "success": True,
     }
     date_of_claim = request["date_of_claim"]
-    filter_bp_readings = []
+    valid_bp_readings = []
     date_of_claim_date = datetime.strptime(date_of_claim, "%Y-%m-%d").date()
 
     if len(request["bp"]) < 1:
@@ -28,22 +28,22 @@ def sufficient_to_autopopulate (request):
     for reading in request["bp"]:
         bp_reading_date = datetime.strptime(reading["date"], "%Y-%m-%d").date()
         if bp_reading_date >= date_of_claim_date - relativedelta(years=1):
-            filter_bp_readings.append(reading)
+            valid_bp_readings.append(reading)
 
-    if len(filter_bp_readings) <= 1 or not bp_readings_meet_date_specs(date_of_claim, filter_bp_readings):
+    if len(valid_bp_readings) <= 1 or not bp_readings_meet_date_specs(date_of_claim, valid_bp_readings):
         predominance_calculation["success"] = False
         return predominance_calculation
 
-    elif len(filter_bp_readings) > 1 and bp_readings_meet_date_specs(date_of_claim, filter_bp_readings):
+    elif len(valid_bp_readings) > 1 and bp_readings_meet_date_specs(date_of_claim, valid_bp_readings):
 
-        if len(filter_bp_readings) == 2:
-            most_recent_reading = sorted(filter_bp_readings, key=lambda d: d["date"])[-1]
+        if len(valid_bp_readings) == 2:
+            most_recent_reading = sorted(valid_bp_readings, key=lambda d: d["date"])[-1]
             predominance_calculation["predominant_diastolic_reading"] = most_recent_reading["diastolic"]
             predominance_calculation["predominant_systolic_reading"] = most_recent_reading["systolic"]
-        elif len(filter_bp_readings) > 2:
-            diastolic_bucket = tally_diastolic_counts(filter_bp_readings)
-            predominance_calculation["predominant_diastolic_reading"] = calculate_reading_from_bucket(diastolic_bucket, filter_bp_readings, True)
-            systolic_bucket = tally_systolic_counts(filter_bp_readings)
-            predominance_calculation["predominant_systolic_reading"] = calculate_reading_from_bucket(systolic_bucket, filter_bp_readings, False)
+        elif len(valid_bp_readings) > 2:
+            diastolic_bucket = tally_diastolic_counts(valid_bp_readings)
+            predominance_calculation["predominant_diastolic_reading"] = calculate_reading_from_bucket(diastolic_bucket, valid_bp_readings, True)
+            systolic_bucket = tally_systolic_counts(valid_bp_readings)
+            predominance_calculation["predominant_systolic_reading"] = calculate_reading_from_bucket(systolic_bucket, valid_bp_readings, False)
 
     return predominance_calculation
